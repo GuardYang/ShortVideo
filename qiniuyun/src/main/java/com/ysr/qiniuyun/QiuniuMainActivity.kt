8b2d5ae4176302cb17089fab850b51ce6cf3be39
yangshirong <yangshirong@log56.com>
2017-11-29 11:43:43 +0800

    七牛云model

---
+++ qiniuyun/src/main/java/com/ysr/qiniuyun/QiuniuMainActivity.kt
@@ -0,0 +1,12 @@
+package com.ysr.qiniuyun
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class QiuniuMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qiuniu_main)
+    }
+}
